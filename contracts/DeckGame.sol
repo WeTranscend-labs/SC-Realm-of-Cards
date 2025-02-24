cbd8319ac2768b0ee9626dee918dc8016c201328
Kaitou <121939559+Quantaphocpython@users.noreply.github.com>
2025-02-24 14:38:30 +0700

    Update DeckGame.sol

--- contracts/DeckGame.sol
+++ contracts/DeckGame.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./DeckType.sol";
 import "./DeckEvent.sol";
+import "./DeckResponse.sol";
 
 contract DeckGame is Ownable {
     using DeckType for DeckType.Card;
@@ -48,5 +49,33 @@ contract DeckGame is Ownable {
     
     function getCard(uint256 _cardId) external view returns (DeckType.Card memory) {
         return cards[_cardId];
+    }
+
+    function getCardsPaginated(uint256 pageIndex, uint256 pageSize)
+        external
+        view
+        returns (DeckResponse.PaginatedCards memory)
+    {
+        require(pageSize > 0, "Page size must be greater than 0");
+
+        uint256 totalElements = nextCardId - 1;
+        uint256 totalPages = (totalElements + pageSize - 1) / pageSize; // Làm tròn lên
+
+        require(pageIndex < totalPages, "Invalid page index");
+
+        uint256 startIndex = pageIndex * pageSize + 1;
+        uint256 endIndex = startIndex + pageSize;
+        if (endIndex > nextCardId) {
+            endIndex = nextCardId;
+        }
+
+        uint256 resultSize = endIndex - startIndex;
+        DeckType.Card[] memory cardsPage = new DeckType.Card[](resultSize);
+
+        for (uint256 i = 0; i < resultSize; i++) {
+            cardsPage[i] = cards[startIndex + i];
+        }
+
+        return DeckResponse.PaginatedCards(cardsPage, totalPages, totalElements);
     }
 }
