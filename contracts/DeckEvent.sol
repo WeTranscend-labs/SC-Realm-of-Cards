0caf8fbc68e646a8f932c27fda6c7813d9ad4389
Kaitou <121939559+Quantaphocpython@users.noreply.github.com>
2025-02-24 18:37:45 +0700

    Update DeckEvent.sol

--- contracts/DeckEvent.sol
+++ contracts/DeckEvent.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.20;
 
 library DeckEvent {
     event CardAdded(uint256 indexed cardId, string name, uint256 attack, uint256 health);
+    event MonsterAdded(uint256 indexed monsterId, uint256 health, uint256 attack);
 }
