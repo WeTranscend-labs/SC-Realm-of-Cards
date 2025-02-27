bde74658d23309994431b6d43d1f532d667d1e51
Kaitou <121939559+Quantaphocpython@users.noreply.github.com>
2025-02-27 14:03:08 +0700

    Update DeckResponse.sol

--- contracts/DeckResponse.sol
+++ contracts/DeckResponse.sol
@@ -4,9 +4,25 @@ pragma solidity ^0.8.20;
 import "./DeckType.sol";
 
 library DeckResponse {
-    struct PaginatedCards {
-        DeckType.Card[] cardsPage;
-        uint256 totalPages;
-        uint256 totalElements;
+    struct CardWithId {
+        uint256 id;        
+        DeckType.Card card;      
+    }
+
+    struct MonsterWithId {
+        uint256 id;           
+        DeckType.Monster monster; 
+    }
+
+    struct PaginatedCardsWithId {
+        CardWithId[] cardsPage;    
+        uint256 totalPages;        
+        uint256 totalElements;     //
+    }
+
+    struct PaginatedMonstersWithId {
+        MonsterWithId[] monstersPage; 
+        uint256 totalPages;        
+        uint256 totalElements;      
     }
 }
